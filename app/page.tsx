5854d26123ee96077ef7315e8f2bc325e8914232
MISBAHULLL <misbahulmunir36843@gmail.com>
2026-01-13 12:51:08 +0700

    ubah bg to react bits

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import dynamic from "next/dynamic";
 import { navItems } from "@/data";
 
 import Hero from "@/components/Hero";
@@ -11,10 +12,29 @@ import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
+// Dynamic import untuk LightPillar (WebGL) - full screen background
+const LightPillar = dynamic(() => import("@/components/ui/LightPillar"), {
+  ssr: false,
+});
+
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-      <div className="max-w-7xl w-full">
+      {/* Full Screen Light Pillar Background */}
+      <div className="fixed inset-0 w-full h-screen overflow-hidden z-0 bg-[#0a0015]">
+        <LightPillar
+          topColor="#9429ff"
+          bottomColor="#ff9e9e"
+          pillarWidth={4.0}
+          pillarHeight={0.3}
+          pillarRotation={25}
+          glowAmount={0.004}
+          intensity={0.8}
+          mixBlendMode="color-dodge"
+        />
+      </div>
+      
+      <div className="max-w-7xl w-full relative z-10">
         <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
@@ -29,3 +49,4 @@ const Home = () => {
 };
 
 export default Home;
+
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,6 @@ import { FaLocationArrow } from "react-icons/fa6";
 import dynamic from "next/dynamic";
 
 import MagicButton from "./MagicButton";
-import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 import ProfileCard from "./ProfileCard";
 
@@ -26,38 +25,6 @@ const Hero = () => {
 
   return (
     <div className="pb-20 pt-36 relative">
-      {/**
-       *  UI: Spotlights
-       *  Link: https://ui.aceternity.com/components/spotlight
-       */}
-      <div>
-        <Spotlight
-          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
-          fill="white"
-        />
-        <Spotlight
-          className="h-[80vh] w-[50vw] top-10 left-full"
-          fill="purple"
-        />
-        <Spotlight
-          className="left-80 top-28 h-[80vh] w-[50vw]"
-          fill="blue"
-        />
-      </div>
-
-      {/**
-       *  Background grid
-       */}
-      <div
-        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
-        absolute top-0 left-0 flex items-center justify-center -z-10"
-      >
-        {/* Radial gradient mask */}
-        <div
-          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
-          bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
-        />
-      </div>
 
       {/* Main Hero Content */}
       <div className="flex flex-col lg:flex-row items-center justify-center gap-10 lg:gap-16 relative my-20 z-10">
